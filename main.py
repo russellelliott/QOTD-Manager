3206d52267b7f201ff73cd548af1c2d5354af6cc
Russell Elliott <russell@quadsquad.net>
2022-05-29 16:20:09 -0700

    able to search for specific question id

--- main.py
+++ main.py
@@ -35,8 +35,10 @@ def help():
     print("PROGRAM COMMANDS")
     print("help: prints list of commands")
     print("exit: exit the program at any stage")
-    print("send: upon entering this command, you have the option to type a message to send, or type \"exit\" to exit.")
+    print("send: upon entering this command, you have the option to add a question to the end of the list, or type \"exit\" to exit.")
+    print("delete: upon entering this command, you have the option to delete a question by specifying its number, or type \"exit\" to exit. ")
 
+questions = "questions.csv" #file that stores questions
 #main function
 if __name__ == "__main__":
     while(True):
@@ -53,14 +55,15 @@ if __name__ == "__main__":
                 break
             #add question id, message, and status to csv file
             #iterate through file, count lines
-            with open("questions.csv", 'r') as fp:
+            #https://pynative.com/python-count-number-of-lines-in-file/
+            with open(questions, 'r') as fp:
                 number = sum(1 for line in fp)
                 print('Total lines:', number)
             fp.close()
             #https://www.delftstack.com/howto/python/python-append-to-csv/
             # First, open the old CSV file in append mode, hence mentioned as 'a'
             # Then, for the CSV file, create a file object
-            with open("questions.csv", "a", newline='') as f_object:
+            with open(questions, "a", newline='') as f_object:
                 #data to add
                 list_data = [number, message]
                 # Pass the CSV  file object to the writer() function
@@ -72,3 +75,32 @@ if __name__ == "__main__":
                 f_object.close()
             #at this point, message is sent
             #makeMessage(message)
+        if(command.lower()=="delete"):
+            print("Type a question number to delete, or type \"exit\" to exit: ")
+            question = int(input())
+            #find that question number in the csv file
+            num = 1 #row number
+            delete = False
+            with open(questions, "r+", newline='') as f_object: #read and write
+                for line in f_object:
+                    #https://stackoverflow.com/questions/4796764/read-file-from-line-2-or-skip-header-row
+                    next(f_object) #skip header row
+                    #print(num)
+                    if(line[0]==str(question)):
+                        print("FOUND")
+                        print(line)
+                        print("Delete this question? yes/no: ")
+                        answer = input()
+                        if(answer.lower() == "yes"):
+                            #proceed with deletion
+                            delete = True
+                            print("Deleting the question")
+                            pass
+                        else:
+                            print("Deletion canceled")
+                    elif(line[0]>str(question) and delete): #only need to change id numbers if deletion is occurring
+                        #fix the id numbers after question is deleted
+                        pass
+                    num+=1
+                        
+            
